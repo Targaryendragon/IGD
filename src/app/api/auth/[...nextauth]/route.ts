e89c03c3015c7d368e8cec0fa95de1f458605e44
Targaryendragon <Targaryenfyc@163.com>
2025-04-01 01:10:40 +0800

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth';
 import { authOptions } from './auth';
 
+// 设置为动态渲染
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
